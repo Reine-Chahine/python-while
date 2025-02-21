288cd4c9d16d66f8d2a423bc2faa895dd7965101
Reine-Chahine <reinechahine3@gmail.com>
2025-02-21 22:04:31 +0200

    adding the rent percentage

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 salary = float(input("Enter a salary:"))
 month = input("Enter the month:")
 saveper =float(input("Enter the percentage of savings:"))
+rentper=float(input("Enter the percentage of  rent:"))
